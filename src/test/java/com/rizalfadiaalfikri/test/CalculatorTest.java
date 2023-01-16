b34468819769b9ff94eae94253a8bfd0be9be1ba
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-16 19:58:29 +0700

    sebelum dan setelah test

--- src/test/java/com/rizalfadiaalfikri/test/CalculatorTest.java
+++ src/test/java/com/rizalfadiaalfikri/test/CalculatorTest.java
@@ -1,10 +1,8 @@
 package com.rizalfadiaalfikri.test;
 
 import com.rizalfadiaalfikri.test.generator.SimpleDisplayNameGenerator;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //@DisplayName("Test untuk Calculator class")
@@ -12,6 +10,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before ALl");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After Each");
+    }
 
     @DisplayName("test skenario untuk method add(integer, integer")
     @Test
