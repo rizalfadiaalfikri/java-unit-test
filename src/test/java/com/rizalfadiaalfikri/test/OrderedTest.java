86cfd7a0668da578876511a1ae2964ba3606d28d
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-18 19:34:09 +0700

    lifcycle test

--- src/test/java/com/rizalfadiaalfikri/test/OrderedTest.java
+++ src/test/java/com/rizalfadiaalfikri/test/OrderedTest.java
@@ -1,27 +1,40 @@
 package com.rizalfadiaalfikri.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+    private int counter = 0;
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+
     @Test
     @Order(1)
     public void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     public void test2() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     public void test1() {
-
+        counter++;
+        System.out.println(counter);
     }
 }
