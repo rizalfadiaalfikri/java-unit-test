104ce185ebc962fba02b072eb13738de983d8c99
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-18 20:22:29 +0700

    dependency injection di test

---
+++ src/test/java/com/rizalfadiaalfikri/test/RandomCalculatorTest.java
@@ -0,0 +1,28 @@
+package com.rizalfadiaalfikri.test;
+
+import com.rizalfadiaalfikri.test.reseolver.RandomParameterResolver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+
+import java.util.Random;
+
+@Extensions({
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class RandomCalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void testRandom(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+        var result = calculator.add(a, b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+
+    }
+}
