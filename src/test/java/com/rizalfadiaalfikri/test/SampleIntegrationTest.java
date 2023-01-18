75c6b6f7dc390aec6957eff0878d3a53c6dca24d
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-18 08:43:20 +0700

    menggunakan tag

---
+++ src/test/java/com/rizalfadiaalfikri/test/SampleIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.rizalfadiaalfikri.test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+@Tags({
+        @Tag("integration-test")
+})
+public class SampleIntegrationTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
