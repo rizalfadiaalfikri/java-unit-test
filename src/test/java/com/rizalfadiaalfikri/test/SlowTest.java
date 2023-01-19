2f4fee382082823cf77ad211cef5339865917ea1
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-19 20:43:07 +0700

    timeout di test

---
+++ src/test/java/com/rizalfadiaalfikri/test/SlowTest.java
@@ -0,0 +1,15 @@
+package com.rizalfadiaalfikri.test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+public class SlowTest {
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow() throws InterruptedException {
+        Thread.sleep(10_000);
+    }
+
+}
