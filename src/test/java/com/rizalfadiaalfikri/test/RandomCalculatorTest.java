da51aad012bb62071d8331d97b7caa3530955e02
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-19 20:33:43 +0700

    test dengan parameter

--- src/test/java/com/rizalfadiaalfikri/test/RandomCalculatorTest.java
+++ src/test/java/com/rizalfadiaalfikri/test/RandomCalculatorTest.java
@@ -4,7 +4,11 @@ import com.rizalfadiaalfikri.test.reseolver.RandomParameterResolver;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Random;
 
 @Extensions({
@@ -54,5 +58,29 @@ public class RandomCalculatorTest extends AbstractCalculatorTest{
 
         Assertions.assertEquals(expected, result);
 
+    }
+
+    @DisplayName("Test calculator")
+    @ParameterizedTest(name = "{displayName} dengan parameter {0}")
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10})
+    void testWithParameter(int value) {
+        var expected = value + value;
+        var result = calculator.add(value, value);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    public static List<Integer> parameterSource() {
+        return List.of(1,2,3,4,5,6,7,8,9,10);
+    }
+
+    @DisplayName("Test calculator")
+    @ParameterizedTest(name = "{displayName} dengan parameter {0}")
+    @MethodSource({"parameterSource"})
+    void testWithMethodSource(Integer value) {
+        var expected = value + value;
+        var result = calculator.add(value, value);
+
+        Assertions.assertEquals(expected, result);
     }
 }
