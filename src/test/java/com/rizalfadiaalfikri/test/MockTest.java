4694346f1a51b694cc3c3c1aae0da3d3c6f5443e
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-01-21 09:55:42 +0700

    pengenalan mocking

---
+++ src/test/java/com/rizalfadiaalfikri/test/MockTest.java
@@ -0,0 +1,19 @@
+package com.rizalfadiaalfikri.test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class MockTest {
+    @Test
+    void testMock() {
+        List<String> list = Mockito.mock(List.class);
+
+        Mockito.when(list.get(0)).thenReturn("Rizal");
+
+        System.out.println(list.get((0)));
+
+        Mockito.verify(list, Mockito.times(1)).get(0);
+    }
+}
